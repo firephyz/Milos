20dd2e440fd80de9a5447efd06efce7a95452b4c
kyle <burgekm@miamioh.edu>
2017-10-18 00:50:44 -0400

    N/A

--- milc/string.scm
+++ milc/string.scm
@@ -1,3 +1,7 @@
+#lang racket
+
+(provide file->string)
+
 ;; Converts a given input port into a
 ;; string of characters
 (define (file->string input)
@@ -5,9 +9,9 @@
   (define (helper input string-builder)
     (let ([char (read-char input)])
       (if (eof-object? char)
-	  (string-builder '())
-	  (begin (string-builder char)
-		 (helper input string-builder)))))
+          (string-builder '())
+          (begin (string-builder char)
+                 (helper input string-builder)))))
   (helper input (make-string-builder)))
 
 ;; Creates an object used to build
@@ -17,33 +21,34 @@
     (define (copy-string index src-max-index dest)
       ;; Copy the src char to the dest
       (string-set! dest
-		   index
-		   (string-ref src index))
+                   index
+                   (string-ref src index))
       ;; Either continue copying or
       ;; return the dest string
       (if (= index src-max-index)
-	  dest
-	  (copy-string (+ index 1) src-max-index dest)))
+          dest
+          (copy-string (+ index 1) src-max-index dest)))
     ;; Copy the string over to a new one
     (let ([src-max-index (- (string-length src) 1)])
       (copy-string 0
-		   src-max-index
-		   (make-string max-index #\space))))
+                   src-max-index
+                   (make-string max-index #\space))))
   ;;  Create the actual string-bulder
   (let* ([index 0]
-	 [max-index 4]
-	 [string (make-string max-index #\space)])
+         [max-index 4]
+         [string (make-string max-index #\space)])
     (lambda (char)
       ;; Return the string if just given null
       (if (null? char)
-	  string
-	  ;; Otherwise, add the character to the string
-	  (begin (string-set! string
-			      index
-			      char)
-		 (set! index (+ index 1))
-		 ;; Expand the string if we need to
-		 (if (= index max-index)
-		     (begin (set! max-index (* max-index 2))
-			    (set! string (expand-string string max-index))))
-		 string)))))
+          string
+          ;; Otherwise, add the character to the string
+          (begin (string-set! string
+                              index
+                              char)
+                 (set! index (+ index 1))
+                 ;; Expand the string if we need to
+                 (if (= index max-index)
+                     (begin (set! max-index (* max-index 2))
+                            (set! string (expand-string string max-index)))
+                     '())
+                 string)))))
---
+++ milc/string.scm~
@@ -0,0 +1,49 @@
+;; Converts a given input port into a
+;; string of characters
+(define (file->string input)
+  ;; Builds up the actual string
+  (define (helper input string-builder)
+    (let ([char (read-char input)])
+      (if (eof-object? char)
+	  (string-builder '())
+	  (begin (string-builder char)
+		 (helper input string-builder)))))
+  (helper input (make-string-builder)))
+
+;; Creates an object used to build
+;; strings from characters
+(define (make-string-builder)
+  (define (expand-string src max-index)
+    (define (copy-string index src-max-index dest)
+      ;; Copy the src char to the dest
+      (string-set! dest
+		   index
+		   (string-ref src index))
+      ;; Either continue copying or
+      ;; return the dest string
+      (if (= index src-max-index)
+	  dest
+	  (copy-string (+ index 1) src-max-index dest)))
+    ;; Copy the string over to a new one
+    (let ([src-max-index (- (string-length src) 1)])
+      (copy-string 0
+		   src-max-index
+		   (make-string max-index #\space))))
+  ;;  Create the actual string-bulder
+  (let* ([index 0]
+	 [max-index 4]
+	 [string (make-string max-index #\space)])
+    (lambda (char)
+      ;; Return the string if just given null
+      (if (null? char)
+	  string
+	  ;; Otherwise, add the character to the string
+	  (begin (string-set! string
+			      index
+			      char)
+		 (set! index (+ index 1))
+		 ;; Expand the string if we need to
+		 (if (= index max-index)
+		     (begin (set! max-index (* max-index 2))
+			    (set! string (expand-string string max-index))))
+		 string)))))
